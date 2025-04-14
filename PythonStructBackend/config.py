02bee22c1c4965d1442376f0b215e8155475b8f4
Neitirite <belohvostiyorlan@gmail.com>
2025-04-14 22:36:30 +0300

    GrownBowl added PythnStructBackend

---
+++ PythonStructBackend/config.py
@@ -0,0 +1,5 @@
+UPLOAD_FOLDER = 'C:/Users/Эрно/PycharmProjects/PythonBackendAPI/front/uploads'
+MAX_FILE_SIZE = 15 * 1024 * 1024 * 1024
+URL_API = "http://127.0.0.1:8012"
+WEBSOCKET_SERVER = "ws://127.0.0.1:2077"
+CHUNK = 1024 * 1024

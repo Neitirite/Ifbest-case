328eb32820dcf165a82cfd6c3f272b4df0add20d
Neitirite <belohvostiyorlan@gmail.com>
2025-04-15 20:16:18 +0300

    Added KotlinS3API

--- KotlinVideoProcessor/src/main/kotlin/Main.kt
+++ KotlinVideoProcessor/src/main/kotlin/Main.kt
@@ -1,15 +1,16 @@
-package org.example
-import Converter
 import java.io.File
 
-var queue: MutableList<String> = mutableListOf()
 val qFile = File("/video_files/queue/queue")
+val s3qFile = File("/video_files/queue/s3queue")
 val videoDirectory = "/video_files/source"
 fun main() {
-
+    if(!s3qFile.exists()){
+        s3qFile.createNewFile()
+    }
     println("QueueManager running...")
     while (true) {
-        queue = qFile.readLines().toMutableList()
+        val queue = qFile.readLines().toMutableList()
+        val s3Queue = s3qFile.readLines().toMutableList()
         if (queue.isNotEmpty()) {
             println(queue)
             val meta = queue.first().split("_")
@@ -21,8 +22,10 @@ fun main() {
             if(conv == 0){
                 queue.remove(queue.first())
                 qFile.writeText(queue.joinToString("\n"))
+                s3Queue.add(id)
+                s3qFile.writeText(s3Queue.joinToString("\n"))
             } else {
-                println("Error processing ${id}")
+                println("Error processing $id")
             }
         }
     }
